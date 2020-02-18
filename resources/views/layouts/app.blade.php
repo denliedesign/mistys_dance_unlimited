9a9a95d589da1ccc899e72d14f9605dd7819f706
denliedesign <customdenlie@gmail.com>
2020-02-18 14:47:39 -0600

    tinyMCE trial 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,6 @@
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
 <script src="js/script.js"></script>
 
-<script src='https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js' referrerpolicy="origin"></script>
 <script>
     tinymce.init({
         selector: '#mytextarea',
