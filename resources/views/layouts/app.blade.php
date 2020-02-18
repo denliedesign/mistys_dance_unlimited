ed587dd95ec1e62c3bdb7c99e3c64160613bccc6
denliedesign <customdenlie@gmail.com>
2020-02-18 15:34:17 -0600

    tinyMCE trial full non-premium

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,9 +63,9 @@
 
     tinymce.init({
         selector: '#performances-textarea',
-        plugins: 'autosave emoticons link lists powerpaste',
-        menubar: 'insert',
-        toolbar: 'restoredraft emoticons link numlist bullist',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+        menubar: 'file edit view insert format tools table help insert',
+        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
     });
 
     tinymce.init({
