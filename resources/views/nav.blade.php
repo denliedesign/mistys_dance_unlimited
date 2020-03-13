c0fcfc8d1e500f004ddd2422e1e96262b73c99a3
denliedesign <customdenlie@gmail.com>
2020-03-13 11:59:07 -0500

    corona

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,3 +1,43 @@
+<div class="accordion" id="accordionExample">
+    <div class="card">
+        <div class="card-header text-center bg-danger" id="headingOne">
+            <h2 class="mb-0">
+                <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    COVID-19 Update
+                </button>
+            </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                Dear MDU family,
+<br><br>
+                At MDU we take care each day to keep faculty and students healthy and safe in our studio including best practices for hygiene and wellness.
+<br><br>
+                While COVID-19 is prominent in the news, the Wisconsin Department of Health Services states that the risk remains low in Wisconsin.
+<br><br>
+                This afternoon State Department of Health Services Secretary Andrea Palm said today her agency recommends the cancellation of all events involving more than 250 people. <strong>That recommendation does not extend to K-12 public schools,</strong> although schools will have to use different “tools and tactics” to keep student gatherings small.
+<br><br>
+                <strong>With our small group instruction format at MDU, and amplified cleaning and disinfecting procedures, we are comfortable continuing to hold classes at this time.</strong>
+<br><br>
+                Advice for prevention continues to be the same as an ordinary cold and flu season. Take normal precautions against this and all types of infection by washing hands for 20 seconds, covering coughs into elbows, and staying home when sick.
+<br><br>
+                In addition to education about ordinary prevention, we are also taking above-ordinary precautions by removing toys in the play area, adjusting curriculum to teach without props, and eliminating circle time for littles (which involves holding hands). And, Miss Sandy has taken it on as her personal mission to Clorox everything...even the plants:).
+<br><br>
+                MDU is committed to continuing to play a proactive role to support and encourage community health, especially for our kids and faculty.  Yes, viruses are contagious. But so is fear. We believe it's important to take precautions and also to remain centered.
+<br><br>
+                We will update you if anything changes regarding schedules, classes, competitions or performances.
+<br><br>
+                In the meantime, we thank you for your support in keeping things as normal as possible for kids during this challenging time.
+<br><br>
+                In partnership,
+<br><br>
+                Miss Misty
+            </div>
+        </div>
+    </div>
+</div>
+
 <div id="top-bar">
     <div class="container">
         <div class="row">
