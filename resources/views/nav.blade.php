c303d570dc44736704a46779b7ce5bd150029f9b
denliedesign <customdenlie@gmail.com>
2020-03-13 18:11:38 -0500

    corona closure

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,36 +3,34 @@
         <div class="card-header text-center bg-danger" id="headingOne">
             <h2 class="mb-0">
                 <button class="btn btn-link text-white" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    COVID-19 Update
+                    COVID-19 Closure Message
                 </button>
             </h2>
         </div>
 
         <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
             <div class="card-body">
-                Dear MDU family,
-<br><br>
-                At MDU we take care each day to keep faculty and students healthy and safe in our studio including best practices for hygiene and wellness.
-<br><br>
-                While COVID-19 is prominent in the news, the Wisconsin Department of Health Services states that the risk remains low in Wisconsin.
-<br><br>
-                This afternoon State Department of Health Services Secretary Andrea Palm said today her agency recommends the cancellation of all events involving more than 250 people. <strong>That recommendation does not extend to K-12 public schools,</strong> although schools will have to use different “tools and tactics” to keep student gatherings small.
-<br><br>
-                <strong>With our small group instruction format at MDU, and amplified cleaning and disinfecting procedures, we are comfortable continuing to hold classes at this time.</strong>
-<br><br>
-                Advice for prevention continues to be the same as an ordinary cold and flu season. Take normal precautions against this and all types of infection by washing hands for 20 seconds, covering coughs into elbows, and staying home when sick.
-<br><br>
-                In addition to education about ordinary prevention, we are also taking above-ordinary precautions by removing toys in the play area, adjusting curriculum to teach without props, and eliminating circle time for littles (which involves holding hands). And, Miss Sandy has taken it on as her personal mission to Clorox everything...even the plants:).
-<br><br>
-                MDU is committed to continuing to play a proactive role to support and encourage community health, especially for our kids and faculty.  Yes, viruses are contagious. But so is fear. We believe it's important to take precautions and also to remain centered.
-<br><br>
-                We will update you if anything changes regarding schedules, classes, competitions or performances.
-<br><br>
-                In the meantime, we thank you for your support in keeping things as normal as possible for kids during this challenging time.
-<br><br>
+                Dear MDU Family,
+                <br><br>
+                At Misty's Dance Unlimited, the health and safety of our faculty and students are our most important priorities.
+                <br><br>
+                This afternoon Gov. Tony Evers has directed the state Department of Health Services to mandated all K-12 schools to shut down beginning Wednesday, March 18.
+                <br><br>
+                We have determined that is it in the best interest of our faculty and families to temporarily suspend all classes for that time period as well. Our last day of classes before the shut down will be the same as the schools - Tuesday, March 17.
+                <br><br>
+                To manage this change, we will be moving our previously schedule spring break up to the first week of the closure (meaning we will not have our ordinary spring break during Easter).
+                <br><br>
+                Also, we run a 32 week dance season, BUT we had scheduled for 33 weeks on the calendar this year! That means we already had one extra week of classes built into our schedule, for which we are VERY grateful.
+                <br><br>
+                <strong>We are planning to resume classes along with the local schools on Monday, April 6 and will offer a week of make up classes immediately following recital to cover the other service days.</strong> If anything changes with that timeline we will let you know.
+                <br><br>
+                <strong>In the meantime, we have been in contact with recital costume suppliers and Viterbo University and recital dates remain unchanged.</strong> We will continue to monitor the situation, and should other changes to our season be needed we will update you accordingly.
+                <br><br>
+                <strong>As always, but especially now, we greatly appreciate your support during these challenging times and so do all of the kids and teachers.</strong>
+                <br><br>
                 In partnership,
-<br><br>
-                Miss Misty
+                <br><br>
+                Misty Lown
             </div>
         </div>
     </div>
