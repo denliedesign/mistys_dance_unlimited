c658f7b7d16ba125b8eaf056bd7f912511b0f766
denliedesign <customdenlie@gmail.com>
2020-06-11 11:48:23 -0500

    SEO

--- resources/views/layouts/2020-app.blade.php
+++ resources/views/layouts/2020-app.blade.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Dance Classes | Onalaska, La Crosse, WI">
+    <meta property="og:description" content="More than just great dancing! At MDU, we offer one of the most complete dance instruction programs in the Midwest and work with students of all ages and abilities.">
+    <meta property="og:image" content="https://mistysdance.com/images/schoolyr1.jpg">
+    <meta property="og:url" content="https://mistysdance.com/index.php">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="description" content="@yield('description', 'From your child’s first class to their graduation performance. MDU has a class for everyone!')">
+
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
@@ -14,6 +21,18 @@
 
 
     <title>@yield('title', 'Misty\'s Dance Unlimited | Onalaska & Holmen, WI')</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#30393e">
+    <meta name="msapplication-TileColor" content="#30393e">
+    <meta name="theme-color" content="#30393e">
+    <meta name="google-site-verification" content="pEj8FQ1at3uraMLV77FgIB99LibvKtfCd2E3Jr8B2Hw" />
+
+    <img src="https://servedby.ipromote.com/ad/?src=pixel_cid&nid=2482&cid=T7VJY1WDF8F&cat=11902" width="1" height="1" border="0" alt=""  style="position: absolute; visibility: hidden;"/>
+
 
 </head>
 <body>
