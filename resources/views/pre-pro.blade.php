0b7cf60c88f170143e073837f301a49be34caf0a
denliedesign <customdenlie@gmail.com>
2020-10-20 11:31:53 -0500

    nutcracker vid update

--- resources/views/pre-pro.blade.php
+++ resources/views/pre-pro.blade.php
@@ -77,7 +77,7 @@
             <div class="row my-5">
                 <div class="col-md">
                     <h5 class="lead"><em>Mariinsky's Nutcraker</em></h5>
-                    <iframe width="100%" height="315" src="https://www.youtube.com/embed/7neCn9nP0bo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe width="100%" height="315" src="https://vimeo.com/85312130" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
                 <div class="col-md">
                     <h5 class="lead"><em>Martha Graham Technique 1975</em></h5>
