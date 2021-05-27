ceead26b74f225f1221faa5bfb4ffe4dc06a8027
denliedesign <customdenlie@gmail.com>
2021-05-27 17:34:58 -0500

    fall schedule

--- resources/views/fall.blade.php
+++ resources/views/fall.blade.php
@@ -12,10 +12,28 @@
         <div class="container py-5">
             <h2 class="text-center">Fall Schedule 2021</h2>
             <p class="text-center">
-                Published May 27
-                <br>Online Registration begins Sat. June 5th at 8 AM
+                Online Registration begins Sat. June 5th at 8 AM
                 <br>Classes begin Wed., Aug. 18th
             </p>
+            <div class="text-center">
+                <a href="/images/mdu-fall-2021.pdf" target="_blank">View PDF</a>
+            </div>
+            <div>
+                <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2">
+                    <img src="/images/mdu-fall-2021-1.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-2.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-3.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-4.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-5.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-6.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-7.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-8.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-9.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-10.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-11.jpg" alt="fall schedule" class="img-fluid p-3">
+                    <img src="/images/mdu-fall-2021-12.jpg" alt="fall schedule" class="img-fluid p-3">
+                </div>
+            </div>
             {{--        <div class="row" id="fall-select">--}}
             {{--            <div class="col">--}}
             {{--                <a href="/fall-studio">--}}
