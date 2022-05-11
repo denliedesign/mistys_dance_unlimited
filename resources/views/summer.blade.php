b0295e87bf8a7420905ae53432b607001b5f6394
denliedesign <customdenlie@gmail.com>
2022-05-11 10:07:53 -0500

    summer dress code

--- resources/views/summer.blade.php
+++ resources/views/summer.blade.php
@@ -22,6 +22,7 @@
             <h3 class="text-center">Summer Calendar 2022</h3>
             <div class="text-center d-flex d-inline justify-content-center">
                 <div class="mx-2 px-2"><a href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank"><button class="btn btn-danger btn-lg">ENROLL NOW!</button></a></div>
+                <div class="mx-2 px-2"><a href="/images/mdu-summer-dress-code-2022.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>
             </div>
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2">
                 <a href="/images/22-summer.pdf"><img src="images/22-summer-2.jpg" alt="" class="img-thumbnail my-3"></a>
