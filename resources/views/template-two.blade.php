6be44851a08989a2b7881a293580e6e6267df49e
denliedesign <customdenlie@gmail.com>
2022-06-14 22:31:57 -0500

    misty txt updates 19

--- resources/views/template-two.blade.php
+++ resources/views/template-two.blade.php
@@ -173,10 +173,10 @@
     <div id="mtjgd-bg" class="text-white">
         <div class="color-filter-blue"></div>
 {{--        <h2 class="secondary-txt">We're About More Than Just Great Dancing™</h2>--}}
-        <h3 style="font-size: 2.75em; position: relative; z-index: 4;" class="fw-bold text-center text-white py-5">We're About More Than Just Great Dancing™</h3>
-        <div>
-            <div class="row tertiary-txt-pos">
-                <div class="col-sm my-4" style="font-size: 1.75em;">
+        <div style="position: relative; z-index: 4;">
+            <h3 style="font-size: 2.75em;" class="fw-bold text-center text-white py-5 mt-5">We're About More Than Just Great Dancing™</h3>
+            <div class="row">
+                <div class="col-sm" style="font-size: 1.75em;">
                     “We don’t teach kids to make great dancers, we teach dance to make great kids.”
                     <br>
                     <em>-Misty Lown</em>
