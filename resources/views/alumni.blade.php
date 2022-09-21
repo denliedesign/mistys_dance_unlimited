ebc923a70d213ffe697886f82eb3c3cb2384b2db
denliedesign <customdenlie@gmail.com>
2022-09-21 14:18:34 -0500

    alumni form

--- resources/views/alumni.blade.php
+++ resources/views/alumni.blade.php
@@ -17,8 +17,12 @@
             <h2 class="pt-5 text-center">Alumni</h2>
             <p class="text-center mb-0">
                 Calling all MDU Alumni!
-                <br>Join us in celebration of our 25th Anniversary Season! Check back this summer for news and updates just for YOU!
+                <br>
+                We are celebrating 25 years at MDU and we want YOU to be a part of it! We are looking for all dancers who have danced at MDU in years past whether you graduated with us or just joined us for a short time.  Please fill out this form to join our mailing list and be in the know about our upcoming celebrations.
             </p>
+            <div class="d-flex justify-content-center mt-4">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfsWTgIHjBVP6vrvBcBr1Xtp2sMtQfuOD0LOG6HKCLGrzOPrw/viewform?embedded=true" width="640" height="1034" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     </div>
 
