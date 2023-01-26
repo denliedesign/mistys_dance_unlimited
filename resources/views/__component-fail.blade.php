405d1dadfabf1eddc8f729a319a8dc1acc727d82
denliedesign <customdenlie@gmail.com>
2023-01-25 19:54:43 -0600

    about us fix

---
+++ resources/views/__component-fail.blade.php
@@ -0,0 +1,121 @@
+<x-instructor image="misty" name="Misty Lown" title="CEO Owner Founder" bio="Miss Misty has been sharing her passion for dance and mentorship with students for 30 years. She is the founder of More Than Just Great Dancing™ affiliated dance studios and “A Chance to Dance Foundation”, a 501(c)3 charitable organization. Misty is passionate about community service and has given over $500,000 in combined cash and studio scholarships over the past 23 years." />
+<x-instructor image="kylie" name="Kylie Williams" title="Studio Director" bio="Miss Kylie returned to MDU in 2015 after graduating from Belhaven University with a Bachelor of Fine Arts in dance and a minor in accounting. She is the Director of our Award Winning Performance Company! She has worked with choreographers associated with Complexions, Ad Deum, Inaside Chicago, and Limon Dance Company. Her favorite dance role was in Pitch Perfect 2." />
+<x-instructor image="shayna" name="Shayna Stellflue" title="General Manager" bio="                        Miss Shayna is the General Manager of Misty’s Dance Unlimited and leads the Safety Team and Recitals. She began teaching at MDU in 1999 and is the creator of “Dancin’ Kids!”, a popular performance group for 7-9 yr. olds. Miss Shayna co-authored a Children’s Curriculum for More Than Just Great Dancing™ and is passionate about instilling confidence in every child." />
+<x-instructor image="kris" name="Kris Nandory" title="Business Manager" bio="                        Kris comes to us with 25 plus years of excelling in managerial careers. She started at MDU in 2013 and currently serves as our Family Services leader and well as coordinating our boutique, Everything Dance. She manages student enrollment, studio emails, family accounts, student attendance, and more! She loves working with families and kids!" />
+<x-instructor image="sandy" name="Sandy Averill" title="Front Desk" bio="                        Miss Sandy has been with MDU since day one! She is our Hospitality Services leader at the front desk as well as Miss Misty’s mom! Miss Sandy is the first friendly face you will see when you come in and always happy to help. She enjoys the warmth and camaraderie of the families and staff at MDU and watching our students grow throughout the years.                    " />
+<x-instructor image="tiffany" name="Tiffany Fischer" title="Admin Assistant" bio="                        Miss Tiffany grew up dancing at MDU and has been teaching since 2008. She is an award-winning choreographer. Miss Tiffany is proficient in all styles of dance and has a passion for seeing students develop their God-given talents in a nurturing environment. Miss Tiffany teaches in our Children's program, Graded Technique program and Performing Groups program. She also is our Senior class advisor." />
+<x-instructor image="kaydra" name="Kaydra Moses" title="Admin Assistant" bio="                        Miss Kaydra Moses is another MDU alumna who enjoys giving back to the next generation. She is currently serving as one of our Performing Groups teachers." />
+<x-instructor image="beth" name="Elisabeth Michelson" title="Costumes" bio="                        Miss Elisabeth has been dancing for 20 years. Her favorite genres include tap, musical theatre, and contemporary. Elisabeth shared her love for performing for many years as a member of MDU’s Performance Company. She is a 2013 graduate of the University of Minnesota with a degree in Elementary Education and Spanish. She has danced with Ingredients Dance Company in Texas and has taught at MDU since 2014." />
+</div>
+<div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-4">
+    <div class="col-sm font-staat text-white d-none d-lg-block" style="font-size: 100px; transform: rotate(-90deg) translate(-50%, -50%); height: 100%;">teachers</div>
+    <div class="col-sm mt-3 d-flex align-items-center font-staat text-white d-block d-lg-none" style="font-size: 100px;">teachers</div>
+    <x-instructor image="addi" name="Addi Berry" title="Dance Instructor" bio="I am a senior at Tomah High School and have been dancing for about 13 years. This is only my third year at Misty’s and my first year teaching. My favorite styles of dance are hip hop, jazz, and contemporary. I hope to continue my dance journey after high school and plan to major in dance in college. " />
+    <x-instructor image="alex" name="Alex Meier" title="Dance Instructor" bio="                        Miss Alex has been teaching at MDU since 2012. She is a ballet and tap specialist with a love for children. She is a graduate of Winona State University and fuels her love of science during the day as a surgical technician for Gundersen Health. She currently serves as the administrative coordinator for Ballet La Crosse.
+" />
+    <x-instructor image="amanda" name="Amanda Schams" title="Dance Instructor" bio="                        Miss Amanda has taught ballet and Pilates across the Midwest for two decades and for 17 years at MDU. She trained in Madison, with the Milwaukee Ballet and the Nutmeg Conservatory. She graduated from the University of Iowa with a dance minor. She has performed professionally with the Madison Ballet and as a guest artist throughout the Midwest.
+" />
+    <x-instructor image="amy" name="Amy Fortner" title="Dance Instructor" bio="                        After doing gymnastics through her sophomore year of college, Amy still has a passion for the sport and loves to teach kids acro and tumbling. Amy has been teaching for many years, and is certified in acro by Acrobatic Arts. She's been teaching at Misty's since 2016.
+" />
+    <x-instructor image="carmina" name="Carmina Stauffer" title="Dance Instructor" bio="Miss Carmina is currently enrolled in Rural Virtual Academy as a Junior in High School. She has been dancing for twelve years with genres consisting of Ballet, Tap and Jazz. She loves working with kids and teaching in the children's program." />
+    <x-instructor image="chloe-jane" name="Chloe Jane Bulman" title="Dance Instructor" bio="Miss Chloe Jane began dancing at MDU at the age of 3, she performed in Ballet La Crosse for 10 years and was a member of our Pre-professional ballet program. She is thrilled to be able to share her love of dance by being a rehearsal assistant with Ballet La Crosse, as well as instructing all ages in ballet, tap, and Steps of Praise." />
+    <x-instructor image="cindy" name="Cindy Anneke" title="Dance Instructor" bio="                        Miss Cindy Anneke is a Preschool dance class specialist, a member of our Adult Tap program. Miss Cindy is also a proud dance mom.
+" />
+    <x-instructor image="denise" name="Denise Meyer" title="Dance Instructor" bio="                        Miss Denise and Miss Misty grew up dancing together at Marilyn School of Dance in La Crosse. Miss Denise is currently a training director at LHI in La Crosse and has loved being back in the dance classroom since 2009. Miss Denise teaches in our Children's program, leads our Darby's Dancer's program, and is a rehearsal assistant for Ballet La Crosse.
+" />
+    <div class="col-sm mt-3 text-white" style="height: 100%;">
+        <div style="position: relative;">
+            <img src="/images-lava/staff-dennis.jpg" alt="staff member" class="img-fluid" style="max-height: 382.50px;">
+            <div id="triangle-bottomleft-staff"></div>
+            <div class="triangle-text font-staat m-2" style="font-size: 30px; line-height: 0.9em; position: absolute; bottom: 0; left: 0;">Mr<br>Dennis Williams</div>
+        </div>
+        <p class="font-syne">
+            <span class="txt-yellow" style="font-size: 1.4em;">Dance Instructor</span><br>
+            Mr. Dennis grew up studying ballet at Maple Conservatory of Dance in California. He graduated from Belhaven University with a BFA in dance and has danced professionally with Ballet Mississippi and Filter Dance Company. Additionally, he has four years of experience in Martial Arts. Mr. Dennis has taught at MDU since 2015.
+        </p>
+    </div>
+    <x-instructor image="emma" name="Emma Calmes" title="Dance Instructor" bio="Ms. Emma grew up dancing at Misty’s and recently graduated from the program. She can’t imagine a world without dance and has always dreamed of sharing her love of dance with others." />
+    <x-instructor image="greta" name="Greta Vollendorf" title="Dance Instructor" bio="Miss Greta Vollendorf is a former competitive dancer from Forthcoming Danspace in Stevens Point, Wisconsin. She is currently studying elementary education and dance at UWL. She is also a 2nd year member of the UWL Dance Team." />
+    <x-instructor image="hannah" name="Hannah Hunt" title="Dance Instructor" bio="Miss Hannah grew up dancing at MDU and performed with Ballet La Crosse for 6 years. She graduated with a degree in Computer Science and works as a Software Engineer. This is her second year teaching as an instructor at MDU and as a rehearsal assistant with Ballet La Crosse." />
+    <x-instructor image="katelyn" name="Katelyn Phelps" title="Dance Instructor" bio="                        Miss Katelyn Phelps is a former competitive dancer from Lakeshore Dance in the Milwaukee area. She is moving to La Crosse to attend school at UW-L." />
+    <x-instructor image="kristina" name="Kristina Schoh" title="Dance Instructor" bio="Miss Kristina is proud to be one of Misty’s original students and one of the first seven students to be on the first ever Performance Company. She taught from 2001-2009 and attests skills acquired at MDU to ultimately help her achieve winning Miss Wisconsin 2009, allowing her to share her passion of dance on the Miss America stage. Miss Kristina is elated to be back for the 25th anniversary of MDU!" />
+    {{--                <x-instructor image="kyla" name="Kyla Betz" title="Dance Instructor" bio="Bio coming soon!" />--}}
+    <x-instructor image="larissa" name="Larissa Oberly" title="Dance Instructor" bio="                        Miss Larissa is a former professional ballerina with the Estonian National Ballet in Europe. She has been teaching for MDU's Pre-Professional Program and our classical performing company, Ballet La Crosse since 2011. Ballet La Crosse was founded by Miss Larissa's late husband and beloved ballet master, Mr. Kennet Oberly." />
+    <x-instructor image="lexi" name="Lexi Fishbein" title="Dance Instructor" bio="I grew up dancing at Summit Dance Shoppe in Plymouth, Minnesota where I danced and competed for 15 years. I now attend Winona State University where I’m studying psychology and continuing with my love for dance by teaching! I help out with the performance company as well as teaching jazz, lyrical and contemporary classes!" />
+{{--    <x-instructor image="melanie" name="Melanie Earl" title="Dance Instructor" bio="                        Miss Melanie Earl taught English and dance abroad is currently teaching Kindergarten at Eagle Bluff in addition to Children's programs at MDU. Miss Melanie is a proud MDU alumna." />--}}
+    <x-instructor image="michelle" name="Michelle Malone" title="Dance Instructor" bio="                        Miss Michelle Malone is an MDU alumna who has performed professionally with Ingredients Dance Company out of Dallas and with a ministry in Brazil. She currently teaches Hip Hop and leads her own company, I & E Dance Company, which is housed at MDU." />
+{{--    <x-instructor image="nicole" name="Nicole Fry" title="Dance Instructor" bio="                        Miss Nicole Fry is a former competitive dancer from Young Dance Academy in the Milwaukee area. She is moving to La Crosse to attend Physician Assistant school at UW-L." />--}}
+    <x-instructor image="olivia" name="Olivia Key" title="Dance Instructor" bio="Miss Olivia grew up dancing at MDU and has been teaching since 2017. Her favorite styles include ballet, lyrical and modern but she is able to teach all styles. Miss Olivia graduated from UW-Stevens Point with a degree in elementary education and dance. She is also a part of our Pre-Professional ballet program and Ballet La Crosse." />
+    <x-instructor image="suzanne" name="Suzanne Swanson Wagner" title="Dance Instructor" bio="                        Miss Suzanne Swanson Wagner was Miss Misty's main teacher growing up and is the former owner of the Marilyn School of Dance in Tomah and Black River Falls. She will be driving in a couple of times per week to share her experience with our students–super exciting!" />
+{{--                <x-instructor image="" name="" title="Dance Instructor" bio="" />--}}
+{{--                madeline beinborn?--}}
+
+
+
+{{--                <div class="col-sm mt-3 text-white" style="height: 100%;">--}}
+{{--                    <div style="position: relative;">--}}
+{{--                        <img src="/images-lava/staff-richard.jpg" alt="staff member" class="img-fluid" style="max-height: 382.50px;">--}}
+{{--                        <div id="triangle-bottomleft-staff"></div>--}}
+{{--                        <div class="triangle-text font-staat m-2" style="font-size: 30px; line-height: 0.9em; position: absolute; bottom: 0; left: 0;">Miss<br>Richard Smith</div>--}}
+{{--                    </div>--}}
+{{--                    <p class="font-syne">--}}
+{{--                        Mr. Richard Smith , an MDU guest teacher favorite, will be doing a technique and teacher training residency with us in the fall. Mr. Richard is a former Giordano Jazz Company2 member and former Artistic Director of Inaside Chicago Dance.--}}
+{{--                    </p>--}}
+{{--                </div>--}}
+
+
+    <script>
+        // an array of original codes
+        var originalCodes = [
+            '<x-instructor image="shayna" name="Shayna Stellflue" title="General Manager" bio="                        Miss Shayna is the General Manager of Misty’s Dance Unlimited and leads the Safety Team and Recitals. She began teaching at MDU in 1999 and is the creator of “Dancin’ Kids!”, a popular performance group for 7-9 yr. olds. Miss Shayna co-authored a Children’s Curriculum for More Than Just Great Dancing™ and is passionate about instilling confidence in every child." />',
+            '<x-instructor image="kris" name="Kris Nandory" title="Business Manager" bio="                        Kris comes to us with 25 plus years of excelling in managerial careers. She started at MDU in 2013 and currently serves as our Family Services leader and well as coordinating our boutique, Everything Dance. She manages student enrollment, studio emails, family accounts, student attendance, and more! She loves working with families and kids!" />',
+            '<x-instructor image="sandy" name="Sandy Averill" title="Front Desk" bio="                        Miss Sandy has been with MDU since day one! She is our Hospitality Services leader at the front desk as well as Miss Misty’s mom! Miss Sandy is the first friendly face you will see when you come in and always happy to help. She enjoys the warmth and camaraderie of the families and staff at MDU and watching our students grow throughout the years.                    " />',
+            '<x-instructor image="tiffany" name="Tiffany Fischer" title="Admin Assistant" bio="                        Miss Tiffany grew up dancing at MDU and has been teaching since 2008. She is an award-winning choreographer. Miss Tiffany is proficient in all styles of dance and has a passion for seeing students develop their God-given talents in a nurturing environment. Miss Tiffany teaches in our Children\'s program, Graded Technique program and Performing Groups program. She also is our Senior class advisor." />',
+            '<x-instructor image="kaydra" name="Kaydra Moses" title="Admin Assistant" bio="                        Miss Kaydra Moses is another MDU alumna who enjoys giving back to the next generation. She is currently serving as one of our Performing Groups teachers." />',
+            '<x-instructor image="beth" name="Elisabeth Michelson" title="Costumes" bio="                        Miss Elisabeth has been dancing for 20 years. Her favorite genres include tap, musical theatre, and contemporary. Elisabeth shared her love for performing for many years as a member of MDU’s Performance Company. She is a 2013 graduate of the University of Minnesota with a degree in Elementary Education and Spanish. She has danced with Ingredients Dance Company in Texas and has taught at MDU since 2014." />',
+        ];
+
+        // an array to store the new formats
+        var newFormats = [];
+
+        for (var i = 0; i < originalCodes.length; i++) {
+            // extract the values from the original code
+            var image = originalCodes[i].match(/image="(.*?)"/)[1];
+            var name = originalCodes[i].match(/name="(.*?)"/)[1];
+            var title = originalCodes[i].match(/title="(.*?)"/)[1];
+            var bio = originalCodes[i].match(/bio="(.*?)"/)[1];
+
+            // insert the values into the new format
+            var newFormat = '<div class="col-sm mt-3 text-white" style="height: 100%;">' +
+                '<div style="position: relative;">' +
+                '<img src="/images-lava/staff-' + image + '.jpg" alt="staff member" class="img-fluid" style="max-height: 382.50px;">' +
+                '<div id="triangle-bottomleft-staff"></div>' +
+                '<div class="triangle-text font-staat m-2" style="font-size: 30px; line-height: 0.9em; position: absolute; bottom: 0; left: 0;">Miss<br>' + name + '</div>' +
+                '</div>' +
+                '<p class="font-syne">' +
+                '<span class="txt-yellow" style="font-size: 1.4em;">' + title + '</span><br>' +
+                bio +
+                '</p>' +
+                '</div>';
+            // append the new format to the newFormats array
+            newFormats.push(newFormat);
+        }
+
+        console.log(newFormats);
+    </script>
+
+    <!--for chatgpt
+
+    can you change this code: "<x-instructor image="misty" name="Misty Lown" title="CEO Owner Founder" bio="Miss Misty has been sharing her passion for dance and mentorship with students for 30 years. She is the founder of More Than Just Great Dancing™ affiliated dance studios and “A Chance to Dance Foundation”, a 501(c)3 charitable organization. Misty is passionate about community service and has given over $500,000 in combined cash and studio scholarships over the past 23 years." />" to this format: "<div class="col-sm mt-3 text-white" style="height: 100%;">
+    <div style="position: relative;">
+        <img src="/images-lava/staff-{{ $image }}.jpg" alt="staff member" class="img-fluid" style="max-height: 382.50px;">
+        <div id="triangle-bottomleft-staff"></div>
+        <div class="triangle-text font-staat m-2" style="font-size: 30px; line-height: 0.9em; position: absolute; bottom: 0; left: 0;">Miss<br>{{ $name }}</div>
+    </div>
+    <p class="font-syne">
+        <span class="txt-yellow" style="font-size: 1.4em;">{{ $title }}</span><br>
+        {{ $bio }}
+    </p>
+</div>" and please fill in the variables. for example since image=misty, please replace {{ $image }} with misty
+
+    -->
