77270b8b83ea87ce7be47c1beac2ad9d10a0ca32
denliedesign <customdenlie@gmail.com>
2024-02-01 20:47:36 -0600

    create article now uses app lava

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,13 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.app-lava')
 
 @section('title', 'Add New Article')
 
 @section('content')
 
-    <div class="banner-wrap">
+    <div class="banner-wrap d-none d-md-block" style="position: relative;">
         <div class="banner"></div>
+        <div class="custom-shape-divider-bottom-1663856745">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z" class="shape-fill"></path>
+            </svg>
+        </div>
     </div>
 
+    <div class="bg-white">
+        <div class="container pb-5 pt-3">
     <div class="row">
         <div class="col-12">
             <h3 class="py-5 text-center">Add New Article</h3>
@@ -26,6 +33,7 @@
         </div>
     </div>
 
-    @include('footer')
+        </div>
+    </div>
 
 @endsection
