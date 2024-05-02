3f0316ef93adf4ed7b8ce3b324231145292f7fab
denliedesign <customdenlie@gmail.com>
2024-05-02 15:26:42 -0500

    hide video show articles

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -34,44 +34,40 @@
 
     <div class="bg-white pb-5">
         <h3 class="text-center py-5">News</h3>
-{{--        @can('update', \App\Article::class)--}}
-{{--            <p class="text-center"><a href="/articles/create">Create New Article</a></p>--}}
-{{--        @endcan--}}
+        @can('update', \App\Article::class)
+            <p class="text-center"><a href="/articles/create">Create New Article</a></p>
+        @endcan
 
         <div class="container" id="news">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 pb-5">
-{{--                @foreach($articles as $article)--}}
+                @foreach($articles as $article)
                     <div class="col-sm p-2">
                         <div class="card" style="max-width: 400px">
-                            <a href="/articles/62">
-{{--                            <a href="/articles/{{ $article->id }}">--}}
-                                <img src="/storage/FRM0ARA8Xl0xPfPIvGuuol1YbJCI3SRqjsyXkmzt.png" class="card-img-top" alt="...">
-{{--                                <img src="{{ asset('/storage/' . $article->newsImage) }}" class="card-img-top" alt="...">--}}
+                            <a href="/articles/{{ $article->id }}">
+                                <img src="{{ asset('/storage/' . $article->newsImage) }}" class="card-img-top" alt="...">
                             </a>
                             <div class="card-body">
-                                <p class="card-title text-uppercase lead fw-bold">May 2024 News</p>
-{{--                                <p class="card-title text-uppercase lead fw-bold">{{ $article->newsTitle }}</p>--}}
-                                <p class="card-text">Spring Recital Updates, Audition Sign Up Reminders and MORE!</p>
-{{--                                <p class="card-text">{!! strip_tags(substr($article->articleTitle, 0, 125)) . '...' !!}</p>--}}
+                                <p class="card-title text-uppercase lead fw-bold">{{ $article->newsTitle }}</p>
+                                <p class="card-text">{!! strip_tags(substr($article->articleTitle, 0, 125)) . '...' !!}</p>
                             </div>
-{{--                            @can('update', \App\Article::class)--}}
-{{--                                <div class="card-footer">--}}
-{{--                                    <div class="admin-controls d-flex justify-content-center align-items-center pb-2">--}}
-{{--                                        <div><a href="/articles/{{ $article->id }}/edit">Edit Article</a></div>--}}
-{{--                                        <div>--}}
-{{--                                            <form action="/articles/{{ $article->id }}" method="POST">--}}
-{{--                                                @method('DELETE')--}}
-{{--                                                @csrf--}}
+                            @can('update', \App\Article::class)
+                                <div class="card-footer">
+                                    <div class="admin-controls d-flex justify-content-center align-items-center pb-2">
+                                        <div><a href="/articles/{{ $article->id }}/edit">Edit Article</a></div>
+                                        <div>
+                                            <form action="/articles/{{ $article->id }}" method="POST">
+                                                @method('DELETE')
+                                                @csrf
 
-{{--                                                <button class="btn btn-danger ml-4" type="submit">Delete</button>--}}
-{{--                                            </form>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            @endcan--}}
+                                                <button class="btn btn-danger ml-4" type="submit">Delete</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endcan
                         </div>
                     </div>
-{{--                @endforeach--}}
+                @endforeach
             </div>
         </div>
     </div>
