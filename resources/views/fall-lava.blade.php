df32b3e216546e832695256708378d4173d7bf9e
denliedesign <customdenlie@gmail.com>
2024-06-04 06:17:05 -0500

    fall schedule

--- resources/views/fall-lava.blade.php
+++ resources/views/fall-lava.blade.php
@@ -14,21 +14,21 @@
         <div class="container pb-5 pt-3">
             <div class="row d-flex align-items-center">
                 <div class="col-sm text-center">
-                    <p class="text-center">
-                        Enrollment has closed for our full dance season which runs mid August through mid May.<br>We invite you to check out our mini sessions.
-                    </p>
+{{--                    <p class="text-center">--}}
+{{--                        Enrollment has closed for our full dance season which runs mid August through mid May.<br>We invite you to check out our mini sessions.--}}
+{{--                    </p>--}}
                     <div class="m-2 px-2">
-                        <a href="/images/mdu-fall-schedule-23-b.pdf" target="_blank"><button class="btn btn-green btn-lg btn-family">VIEW CLASS SCHEDULE PDF</button></a>
+                        <a href="/images/24-25-fall-schedule.pdf" target="_blank"><button class="btn btn-green btn-lg btn-family">VIEW CLASS SCHEDULE PDF</button></a>
                     </div>
 {{--                    <div class="m-2 px-2">--}}
 {{--                        <a href="/images-lava/dress-code-22-23.pdf" target="_blank"><button class="text-white btn btn-blue btn-lg btn-family">VIEW DRESS CODE</button></a>--}}
 {{--                    </div>--}}
-{{--                    <div class="m-2 px-2">--}}
-{{--                        <a href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank"><button class="text-white btn btn-pink btn-lg btn-family">REGISTER NOW</button></a>--}}
-{{--                    </div>--}}
+                    <div class="m-2 px-2">
+                        <a href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank"><button class="text-white btn btn-pink btn-lg btn-family">REGISTER NOW</button></a>
+                    </div>
                 </div>
                 <div class="col-sm">
-                    <h3 class="font-staat" style="font-size: 100px; line-height: 0.9em;">2023-24 Class Schedule</h3>
+                    <h3 class="font-staat" style="font-size: 100px; line-height: 0.9em;">2024-25 Class Schedule</h3>
                     <p class="font-syne" style="font-size: 22px;">
 {{--                        Online Enrollment has begun! Join the fun – YOU belong HERE!--}}
 {{--                        Online enrollment begins Saturday, June 10th. Join the fun - YOU belong HERE!--}}
@@ -36,18 +36,18 @@
                 </div>
             </div>
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2">
-                <img src="/images/mdu-fall-schedule-23-b_page-0001.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/mdu-fall-schedule-23-b_page-0002.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/mdu-fall-schedule-23-b_page-0003.jpg" alt="fall schedule" class="img-fluid p-1" id="age-2-4">
-                <img src="/images/mdu-fall-schedule-23-b_page-0004.jpg" alt="fall schedule" class="img-fluid p-1" id="age-k-1st">
-                <img src="/images/mdu-fall-schedule-23-b_page-0005.jpg" alt="fall schedule" class="img-fluid p-1" id="age-2nd-3rd">
-                <img src="/images/mdu-fall-schedule-23-b_page-0006.jpg" alt="fall schedule" class="img-fluid p-1" id="age-graded">
-                <img src="/images/mdu-fall-schedule-23-b_page-0007.jpg" alt="fall schedule" class="img-fluid p-1" id="age-teen">
-                <img src="/images/mdu-fall-schedule-23-b_page-0008.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/mdu-fall-schedule-23-b_page-0009.jpg" alt="fall schedule" class="img-fluid p-1" id="age-guys">
-                <img src="/images/mdu-fall-schedule-23-b_page-0010.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/mdu-fall-schedule-23-b_page-0011.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/mdu-fall-schedule-23-b_page-0012.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/24-25-fall-schedule_page-0001.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/24-25-fall-schedule_page-0002.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/24-25-fall-schedule_page-0003.jpg" alt="fall schedule" class="img-fluid p-1" id="age-2-4">
+                <img src="/images/24-25-fall-schedule_page-0004.jpg" alt="fall schedule" class="img-fluid p-1" id="age-k-1st">
+                <img src="/images/24-25-fall-schedule_page-0005.jpg" alt="fall schedule" class="img-fluid p-1" id="age-2nd-3rd">
+                <img src="/images/24-25-fall-schedule_page-0006.jpg" alt="fall schedule" class="img-fluid p-1" id="age-graded">
+                <img src="/images/24-25-fall-schedule_page-0007.jpg" alt="fall schedule" class="img-fluid p-1" id="age-teen">
+                <img src="/images/24-25-fall-schedule_page-0008.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/24-25-fall-schedule_page-0009.jpg" alt="fall schedule" class="img-fluid p-1" id="age-guys">
+{{--                <img src="/images/24-25-fall-schedule_page-0010.jpg" alt="fall schedule" class="img-fluid p-1">--}}
+{{--                <img src="/images/24-25-fall-schedule_page-0011.jpg" alt="fall schedule" class="img-fluid p-1">--}}
+{{--                <img src="/images/24-25-fall-schedule_page-0012.jpg" alt="fall schedule" class="img-fluid p-1">--}}
             </div>
         </div>
     </div>
--- resources/views/nav-lava.blade.php
+++ resources/views/nav-lava.blade.php
@@ -39,12 +39,12 @@
                                 <li class="nav-item mx-2">
                                     <a class="nav-link" href="/summer">SUMMER</a>
                                 </li>
+                <li class="nav-item mx-2">
+                    <a class="nav-link" href="/fall">FALL</a>
+                </li>
 
 {{--                <li class="nav-item mx-2">--}}
 {{--                    <a class="nav-link" href="/community-programming">PROGRAMS</a>--}}
-{{--                </li>--}}
-{{--                <li class="nav-item mx-2">--}}
-{{--                    <a class="nav-link" href="/fall">SCHEDULE</a>--}}
 {{--                </li>--}}
                 <li class="nav-item mx-2">
                     <a class="nav-link" href="/articles">NEWS</a>
