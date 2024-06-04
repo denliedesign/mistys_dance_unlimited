1c3d768cc80387836022bf014f4a71effcd9eedf
denliedesign <customdenlie@gmail.com>
2024-06-04 06:28:40 -0500

    reset password update route

--- routes/web.php
+++ routes/web.php
@@ -525,4 +525,4 @@ Route::post('register', [RegisterController::class, 'register']);
 Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ResetPasswordController::class, 'reset']);
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
