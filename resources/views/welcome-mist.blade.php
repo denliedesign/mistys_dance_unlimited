c545415a8fed76731f138e86dadab990a3c6bf15
denliedesign <customdenlie@gmail.com>
2024-06-18 14:54:43 -0500

    pills go back to md media query not lg

--- resources/views/welcome-mist.blade.php
+++ resources/views/welcome-mist.blade.php
@@ -241,7 +241,7 @@
 
     <div class="" style="height: 125px;"></div>
 
-    <section class="d-block d-lg-none">
+    <section class="d-block d-md-none">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
