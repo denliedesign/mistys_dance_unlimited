aa2d99c744811c356d2cde0f5d160ed6c68e1053
denliedesign <customdenlie@gmail.com>
2024-08-06 15:13:10 -0500

    dress code and new program guide

--- resources/views/fall-mist.blade.php
+++ resources/views/fall-mist.blade.php
@@ -6,17 +6,23 @@
             <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2024-2025</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Class Schedule</h1>
             <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank">Enroll Now!</a></div>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="/images/24-25-fall-b.pdf" target="_blank">View Class Schedule PDF</a></div>
+{{--            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="/images/24-25-fall-b.pdf" target="_blank">View Class Schedule PDF</a></div>--}}
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="/images-mist/dress-code-24-25.png" target="_blank">Dress Code</a></div>
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 mt-4">
-                <img src="/images/24-25-fall-b1.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/24-25-fall-b2.jpg" alt="fall schedule" class="img-fluid p-1" id="ab">
-                <img src="/images/24-25-fall-b3.jpg" alt="fall schedule" class="img-fluid p-1" id="c">
-                <img src="/images/24-25-fall-b4.jpg" alt="fall schedule" class="img-fluid p-1" id="d">
-                <img src="/images/24-25-fall-b5.jpg" alt="fall schedule" class="img-fluid p-1" id="e">
-                <img src="/images/24-25-fall-b6.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/24-25-fall-b7.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/24-25-fall-b8.jpg" alt="fall schedule" class="img-fluid p-1" id="age-guys">
-                <img src="/images/24-25-fall-b9.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/1.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/2.png" alt="fall schedule" class="img-fluid p-1" id="ab">
+                <img src="/images-mist/3.png" alt="fall schedule" class="img-fluid p-1" id="c">
+                <img src="/images-mist/4.png" alt="fall schedule" class="img-fluid p-1" id="d">
+                <img src="/images-mist/5.png" alt="fall schedule" class="img-fluid p-1" id="e">
+                <img src="/images-mist/6.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/7.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/8.png" alt="fall schedule" class="img-fluid p-1" id="age-guys">
+                <img src="/images-mist/9.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/10.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/11.png" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images-mist/12.png" alt="fall schedule" class="img-fluid p-1">
+{{--                <img src="/images-mist/13.png" alt="fall schedule" class="img-fluid p-1">--}}
+                <img src="/images-mist/14.png" alt="fall schedule" class="img-fluid p-1">
                 {{--                <img src="/images/24-25-fall-schedule_page-0010.jpg" alt="fall schedule" class="img-fluid p-1">--}}
                 {{--                <img src="/images/24-25-fall-schedule_page-0011.jpg" alt="fall schedule" class="img-fluid p-1">--}}
                 {{--                <img src="/images/24-25-fall-schedule_page-0012.jpg" alt="fall schedule" class="img-fluid p-1">--}}
