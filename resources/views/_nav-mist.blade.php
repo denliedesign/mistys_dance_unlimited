ea812b691b5f10b8752616673c187ae2d65b134f
denliedesign <customdenlie@gmail.com>
2025-03-12 19:48:03 -0500

    childrens summer

--- resources/views/_nav-mist.blade.php
+++ resources/views/_nav-mist.blade.php
@@ -26,9 +26,9 @@
 {{--                <a class="nav-link" href="/promotions">Winter/Spring</a>--}}
                 <a class="nav-link" href="/promotions">Upcoming Classes</a>
             </li>
-{{--            <li class="nav-item mx-2">--}}
-{{--                <a class="nav-link" href="/summer">Summer</a>--}}
-{{--            </li>--}}
+            <li class="nav-item mx-2">
+                <a class="nav-link" href="/summer">Summer</a>
+            </li>
 {{--            <li class="nav-item mx-2">--}}
 {{--                <a class="nav-link" href="/fall">Fall</a>--}}
 {{--            </li>--}}
--- resources/views/desktop/home/_navigation.blade.php
+++ resources/views/desktop/home/_navigation.blade.php
@@ -17,9 +17,9 @@
 {{--                    <a class="nav-link text-dark" href="/promotions">Winter/Spring</a>--}}
                     <a class="nav-link text-dark" href="/promotions">Upcoming Classes</a>
                 </li>
-{{--                <li class="nav-item hover-link mx-2">--}}
-{{--                    <a class="nav-link text-dark" href="/summer">Summer</a>--}}
-{{--                </li>--}}
+                <li class="nav-item hover-link mx-2">
+                    <a class="nav-link text-dark" href="/summer">Summer</a>
+                </li>
 {{--                <li class="nav-item hover-link mx-2">--}}
 {{--                    <a class="nav-link text-dark" href="/fall">Fall</a>--}}
 {{--                </li>--}}
