318111c9976022b280b0be6ee8f8d4dfa7ad439f
denliedesign <customdenlie@gmail.com>
2025-04-01 15:51:00 -0500

    summer dress code up

--- resources/views/summer-mist.blade.php
+++ resources/views/summer-mist.blade.php
@@ -9,7 +9,9 @@
             <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2025</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Children's Summer Dance Classes</h1>
             <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank">Enroll Now!</a></div>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact ms-3"><a class="text-white text-decoration-none" href="/images/2025-summer.pdf" target="_blank">View PDF</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact mx-3"><a class="text-white text-decoration-none" href="/images/2025-summer.pdf" target="_blank">View PDF</a></div>
+            <div style="color: white; font-size: 20px;" class="btn poppins fw-bold"><a href="/images/25-dress-code.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>
+
 
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1">
                 <img src="images/2025-summer-a.jpg" alt="summer dance classes" class="my-3">
@@ -34,7 +36,6 @@
             {{--            <div class="text-center d-flex d-inline justify-content-center my-3">--}}
 {{--                                <div class="mx-2 px-2"><a href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank"><button class="btn btn-danger btn-lg">Enroll Now!</button></a></div>--}}
 {{--                                <div class="mx-2 px-2"><a href="/images/graded-summer-dance-classes-2024.pdf" target="_blank"><button class="btn btn-danger btn-lg">View PDF</button></a></div>--}}
-{{--                <div class="mx-2 px-2"><a href="/images/mdu-summer-dress-code-2023.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>--}}
 {{--            </div>--}}
         </div>
     </div>
