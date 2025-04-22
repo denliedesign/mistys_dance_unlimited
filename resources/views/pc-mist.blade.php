8595efe8620786f5e36d3c30bfbbb9c4a6ff3b02
denliedesign <customdenlie@gmail.com>
2025-04-22 10:20:36 -0500

    pg auditions

--- resources/views/pc-mist.blade.php
+++ resources/views/pc-mist.blade.php
@@ -4,10 +4,16 @@
 
     <div class="bg-white py-5">
         <div class="container" id="adult">
-            <p class="text-muted pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2024-25</p>
+            <p class="text-muted pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2025-26</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Performing Groups, Ballet La Crosse & Pre-Professional Ballet Auditions</h1>
             <p class="poppins" style="font-size: 20px;">Does your dancer LOVE to perform?</p>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="https://docs.google.com/forms/d/e/1FAIpQLSdHrOHpexwXLiz7pSUkw1zNYjEBVtlbwEeyyyTe3M2frzA4Bg/viewform?usp=sf_link" target="_blank">Click to learn more!</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact mb-3"><a class="text-white text-decoration-none" href="
+            https://docs.google.com/forms/d/e/1FAIpQLSdX8zV6VRqDDCjd9jm0fq8iMjScw69cdCIZYajf22MXK5Z9vw/viewform?usp=dialog
+" target="_blank">Click to learn more!</a></div>
+            <div>
+                <img src="/images/cinderella-auditions-25.jpg" alt="pg flyer" class="img-fluid mb-3">
+                <img src="/images/pg-auditions-25.jpg" alt="pg flyer" class="img-fluid">
+            </div>
         </div>
     </div>
 
