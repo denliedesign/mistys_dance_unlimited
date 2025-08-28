1071bfee07ff733c88d8414f78c42688b1188786
denliedesign <customdenlie@gmail.com>
2025-08-27 22:06:03 -0500

    gtm

--- resources/views/layouts/app-mist.blade.php
+++ resources/views/layouts/app-mist.blade.php
@@ -35,6 +35,13 @@
 
         gtag('config', 'G-F3Q8GM09JM');
     </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NLRPC4K3');</script>
+    <!-- End Google Tag Manager -->
 
     <img src="https://servedby.ipromote.com/ad/?src=pixel_cid&nid=2482&cid=T7VJY1WDF8F&cat=11902" width="1" height="1" border="0" alt="" style="position: absolute; visibility: hidden;"/>
 
@@ -61,6 +68,10 @@
     {{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLRPC4K3"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @if(session()->has('message'))
     <div class="alert alert-success mb-0" role="alert">
         <strong>Success</strong> {{ session()->get('message') }}
