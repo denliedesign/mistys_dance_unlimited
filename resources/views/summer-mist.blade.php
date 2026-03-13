734aed7cfc81d217ace602f8be3d2ee2ce377d90
denliedesign <customdenlie@gmail.com>
2026-03-12 21:47:52 -0500

    summer

--- resources/views/summer-mist.blade.php
+++ resources/views/summer-mist.blade.php
@@ -6,35 +6,32 @@
 
     <div class="bg-white py-5 poppins">
         <div class="container" id="summer">
-            <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2025</p>
+            <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2026</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Children's Summer Dance Classes</h1>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank">Enroll Now!</a></div>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact mx-3"><a class="text-white text-decoration-none" href="/images/2025-summer.pdf" target="_blank">View PDF</a></div>
-            <div style="color: white; font-size: 20px;" class="btn poppins fw-bold"><a href="/images/25-dress-code.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" disabled>Enroll Wednesday, March 18!</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact mx-3"><a class="text-white text-decoration-none" href="/images/26-summer.pdf" target="_blank">View PDF</a></div>
+            <div style="color: white; font-size: 20px;" class="btn poppins fw-bold"><a href="/images/dress-code-25-26.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>
 
 
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1">
-                <img src="images/2025-summer-a.jpg" alt="summer dance classes" class="my-3">
-                <img src="images/2025-summer-b.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-kids.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-kids-2.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-kids-glossary.jpg" alt="summer dance classes" class="my-3">
             </div>
 
                         <hr class="my-5">
-            <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2025</p>
+            <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2026</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Graded Summer Dance Classes</h1>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" target="_blank">Enroll Now!</a></div>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact ms-3"><a class="text-white text-decoration-none" href="/images/summer-graded-6-4-25.pdf" target="_blank">View PDF</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" disabled>Enroll Wednesday, March 18!</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact ms-3"><a class="text-white text-decoration-none" href="/images/26-summer.pdf" target="_blank">View PDF</a></div>
 
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1">
-                <img src="images/summer-5-7-25_page-0001.jpg" alt="summer dance classes" class="my-3">
-                <img src="images/summer-5-7-25_page-0002.jpg" alt="summer dance classes" class="my-3">
-                <img src="images/summer-june-classes-6-1-25.jpg" alt="summer dance classes" class="my-3">
-{{--                <img src="images/summer-5-7-25_page-0003.jpg" alt="summer dance classes" class="my-3">--}}
-                <img src="images/summer-5-7-25_page-0004.jpg" alt="summer dance classes" class="my-3">
-                <img src="images/summer-july-classes-6-1-25.jpg" alt="summer dance classes" class="my-3">
-{{--                <img src="images/summer-5-7-25_page-0005.jpg" alt="summer dance classes" class="my-3">--}}
-                <img src="images/summer-5-7-25_page-0006.jpg" alt="summer dance classes" class="my-3">
-                <img src="images/summer-august-classes-6-1-25.jpg" alt="summer dance classes" class="my-3">
-{{--                <img src="images/summer-5-7-25_page-0007.jpg" alt="summer dance classes" class="my-3">--}}
+                <img src="images/26-summer.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-2.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-3.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-4.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-5.jpg" alt="summer dance classes" class="my-3">
+                <img src="images/26-summer-6.jpg" alt="summer dance classes" class="my-3">
             </div>
 
             {{--            <div class="text-center d-flex d-inline justify-content-center my-3">--}}
