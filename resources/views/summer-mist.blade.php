87ab5ca946756b19a51c9c374435e24d85972a9c
denliedesign <customdenlie@gmail.com>
2026-06-06 07:55:12 -0500

    enroll buttons change

--- resources/views/summer-mist.blade.php
+++ resources/views/summer-mist.blade.php
@@ -8,7 +8,7 @@
         <div class="container" id="summer">
             <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2026</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Children's Summer Dance Classes</h1>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" disabled>Enroll Wednesday, March 18!</a></div>
+            <div style="color: white; font-size: 20px;" class="btn btn-danger poppins fw-bold btn-interact"><a class="text-white text-decoration-none">Enroll Now!</a></div>
             <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact mx-3"><a class="text-white text-decoration-none" href="/images/26-summer-3-18.pdf" target="_blank">View PDF</a></div>
             <div style="color: white; font-size: 20px;" class="btn poppins fw-bold"><a href="/images/dress-code-25-26.pdf" target="_blank"><button class="btn btn-danger btn-lg">Summer Dress Code</button></a></div>
 
@@ -22,7 +22,7 @@
                         <hr class="my-5">
             <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2026</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Graded Summer Dance Classes</h1>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" disabled>Enroll Wednesday, March 18!</a></div>
+            <div style="color: white; font-size: 20px;" class="btn btn-danger poppins fw-bold btn-interact"><a class="text-white text-decoration-none" disabled>Enroll Now!</a></div>
             <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact ms-3"><a class="text-white text-decoration-none" href="/images/26-summer-3-18.pdf" target="_blank">View PDF</a></div>
 
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-1">
