5de031be35089ccfe49113f1e927c11b97c7cfba
denliedesign <customdenlie@gmail.com>
2026-06-28 16:40:52 -0500

    program guide update

--- resources/views/fall-mist.blade.php
+++ resources/views/fall-mist.blade.php
@@ -6,23 +6,23 @@
             <p class="pb-0 mb-0 poppins text-muted" style="font-size: 30px;">MDU 2026-2027</p>
             <h1 class="born-7 poppins fw-bold p-0 m-0" style="font-size: 45px;">Class Schedule</h1>
             <div style="color: white; font-size: 20px;" class="btn btn-danger poppins fw-bold btn-interact me-3 mt-3"><a href="https://app.thestudiodirector.com/mistysdance/portal.sd?page=Login" class="text-white text-decoration-none" target="_blank">Enroll Now!</a></div>
-            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="/images/6-4-26-fall-schedule.pdf" target="_blank">View Class Schedule PDF</a></div>
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a class="text-white text-decoration-none" href="/images/6-28-26-program-guide.pdf" target="_blank">View Class Schedule PDF</a></div>
             <div style="border: 2px solid #008CC0; color: #008CC0; background: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a style="color: #008CC0;"  class="text-decoration-none" href="/images/26-27-dress-code.pdf" target="_blank">Dress Code</a></div>
             <div style="border: 2px solid #008CC0; color: #008CC0; background: white; font-size: 20px;" class="btn poppins fw-bold btn-interact me-3 mt-3"><a style="color: #008CC0;" class="text-decoration-none" href="/images/26-27-policy.pdf" target="_blank">Studio Policies</a></div>
             <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1 row-cols-lg-2 mt-4">
-                <img src="/images/6-4-26-fall-schedule_page-0001.jpg" alt="fall schedule" class="img-fluid p-1" id="ab">
-                <img src="/images/6-4-26-fall-schedule_page-0002.jpg" alt="fall schedule" class="img-fluid p-1" id="ab">
-                <img src="/images/6-4-26-fall-schedule_page-0003.jpg" alt="fall schedule" class="img-fluid p-1" id="c">
-                <img src="/images/6-4-26-fall-schedule_page-0004.jpg" alt="fall schedule" class="img-fluid p-1" id="d">
-                <img src="/images/6-4-26-fall-schedule_page-0005.jpg" alt="fall schedule" class="img-fluid p-1" id="e">
-                <img src="/images/6-4-26-fall-schedule_page-0006.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/6-4-26-fall-schedule_page-0007.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/6-4-26-fall-schedule_page-0008.jpg" alt="fall schedule" class="img-fluid p-1" id="age-guys">
-                <img src="/images/6-4-26-fall-schedule_page-0009.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/6-4-26-fall-schedule_page-0010.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/6-4-26-fall-schedule_page-0011.jpg" alt="fall schedule" class="img-fluid p-1">
-                <img src="/images/6-4-26-fall-schedule_page-0012.jpg" alt="fall schedule" class="img-fluid p-1">
-{{--                <img src="/images/6-4-26-fall-schedule_page-00013.jpg" alt="fall schedule" class="img-fluid p-1">--}}
+                <img src="/images/6-28-26-program-guide-1.jpg" alt="fall schedule" class="img-fluid p-1" id="ab">
+                <img src="/images/6-28-26-program-guide-2.jpg" alt="fall schedule" class="img-fluid p-1" id="ab">
+                <img src="/images/6-28-26-program-guide-3.jpg" alt="fall schedule" class="img-fluid p-1" id="c">
+                <img src="/images/6-28-26-program-guide-4.jpg" alt="fall schedule" class="img-fluid p-1" id="d">
+                <img src="/images/6-28-26-program-guide-5.jpg" alt="fall schedule" class="img-fluid p-1" id="e">
+                <img src="/images/6-28-26-program-guide-6.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/6-28-26-program-guide-7.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/6-28-26-program-guide-8.jpg" alt="fall schedule" class="img-fluid p-1" id="age-guys">
+                <img src="/images/6-28-26-program-guide-9.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/6-28-26-program-guide-10.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/6-28-26-program-guide-11.jpg" alt="fall schedule" class="img-fluid p-1">
+                <img src="/images/6-28-26-program-guide-12.jpg" alt="fall schedule" class="img-fluid p-1">
+{{--                <img src="/images/6-28-26-program-guide-13.jpg" alt="fall schedule" class="img-fluid p-1">--}}
             </div>
         </div>
     </div>
