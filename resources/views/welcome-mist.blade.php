f0cee155932b27ec5605c993ab96a90734c39109
denliedesign <customdenlie@gmail.com>
2026-09-08 12:20:42 -0500

    unhide cta

--- resources/views/welcome-mist.blade.php
+++ resources/views/welcome-mist.blade.php
@@ -10,7 +10,7 @@
         <h1 class="born-1 poppins fw-bold p-0 m-0" style="font-size: 45px; transform: translateY(45px); opacity: 0;">You belong here <span style="color: #ED174D;"><ion-icon name="heart"></ion-icon></span></h1>
         <div class="born-2" style="transform: translateY(45px); opacity: 0;">
             <h2 class="poppins p-0 m-0 text-muted my-3" style="font-size: 20px;">Your child will fall in love with dance with our classes for ages 2-18!</h2>
-{{--            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="/fall">Find your class</a></div>--}}
+            <div style="background: #008CC0; color: white; font-size: 20px;" class="btn poppins fw-bold btn-interact"><a class="text-white text-decoration-none" href="/fall">Find your class</a></div>
         </div>
     </div>
     @include('/desktop/home/_cta')
